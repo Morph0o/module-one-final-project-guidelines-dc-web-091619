3756c2190abc7f1cb9f9d58fdfa21783d89472eb
Mickey <detrodoniv@yahoo.com>
2019-09-30 17:43:05 -0400

    edit

--- lib/models/recipe.rb
+++ lib/models/recipe.rb
@@ -1,6 +1,6 @@
 class Recipe < ActiveRecord::Base
 has many :recipe_ingredients 
-has many : ingredients, through: :recipe_ingredients
+has many :ingredients, through: :recipe_ingredients
 attr_reader :name,:ingredients,:description
 
 def initialize(name,description)
