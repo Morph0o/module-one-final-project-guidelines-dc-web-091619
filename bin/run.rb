3d07ca2a688ed0f0b1b982fad3d269e60a797de4
Mickey <detrodoniv@yahoo.com>
2019-10-01 14:48:44 -0400

    config seeds

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,11 @@
 require_relative '../config/environment'
+require_relative '../lib/models/recipe.rb'
+require_relative '../lib/models/ingredient.rb'
+require_relative '../lib/models/recipe_ingredients.rb'
+
+puts "Welcome to the Fridge"
+puts "this is a app where you find what you can make based on ingredients you have"
+puts "please input ingredients"
+user_input = gets.chomp
 
 
-
-puts "HELLO WORLD"
