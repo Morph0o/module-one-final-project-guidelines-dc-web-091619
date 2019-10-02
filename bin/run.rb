7783fd7668577a3e9ec87909f81ff4aa5432faae
Mickey <detrodoniv@yahoo.com>
2019-10-02 15:58:48 -0400

    spelling error fix

--- bin/run.rb
+++ bin/run.rb
@@ -6,7 +6,7 @@ cli = Cli.new
 
     puts "Welcome to the Fridge"
     puts "this is a app where you find what you can make based on ingredients you have"
-    puts "please input ingredients with a coma between"
+    puts "please input ingredients with a comma between"
     user_input = gets.chomp
     list = user_input.split(",")        
     cli.prompt_ingredient
