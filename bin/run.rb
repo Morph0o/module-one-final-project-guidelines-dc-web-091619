1bd85669f4a3dc00408e51113c61365aea95113d
Mickey <detrodoniv@yahoo.com>
2019-10-02 12:14:53 -0400

    add interface

--- bin/run.rb
+++ bin/run.rb
@@ -2,17 +2,18 @@ require_relative '../config/environment'
 require_relative '../lib/models/recipe.rb'
 require_relative '../lib/models/ingredient.rb'
 require_relative '../lib/models/recipe_ingredients.rb'
+cli = Cli.new
 
-puts "Welcome to the Fridge"
-puts "this is a app where you find what you can make based on ingredients you have"
-puts "please input ingredients with a coma between"
-user_input = gets.chomp
-list = user_input.split(",")
-puts "Would you like to add more ingredients? PRESS I"
-puts "Would you like recipes? PRESS R"
+    puts "Welcome to the Fridge"
+    puts "this is a app where you find what you can make based on ingredients you have"
+    puts "please input ingredients with a coma between"
+    user_input = gets.chomp
+    list = user_input.split(",")        
+    cli.prompt_ingredient
+    decision = gets.chomp
+   cli.options(decision,list) 
 
 
 
 
-binding.pry
 0
---
+++ lib/models/cli.rb
@@ -0,0 +1,37 @@
+require_relative './recipe.rb'
+require_relative './ingredient.rb'
+require_relative './recipe_ingredients.rb'
+
+class Cli
+    
+    def prompt_ingredient
+        puts "Would you like to add more ingredients? PRESS I"
+        puts "Would you like recipes? PRESS R"
+    end
+   
+    def options(decision,list)
+        if decision.downcase == "i"
+            puts "What ingredient would you like to add?"
+            user_list = list << gets.chomp
+            self.prompt_ingredient
+            decision = gets.chomp
+            self.options(decision,user_list)
+        elsif decision.downcase =="r"
+
+           found_recipes = Recipe.find_recipes(list)
+           found_recipes.each do |fr|
+            puts "Name: #{fr.name}
+             Description: #{fr.description}" 
+           end
+        else
+            puts "That is not a command. Please enter 'I' to add more Ingredients or 'R' to find Recipes"
+            decision = gets.chomp
+            self.options(decision,list)
+        end  
+    
+    end
+
+
+
+end
+
